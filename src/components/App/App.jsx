56a06d3d5bd9643f3c4be60a769098e691fb1f3f
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-07-02 22:47:09 +0300

    Rewrite Appr to Class Component

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,15 +1,28 @@
+import { Component } from 'react';
+
 import ImageGallery from 'components/ImageGallery/ImageGallery';
 import Searchbar from 'components/Searchbar/Searchbar';
-import Loader from 'components/Loader/Loader';
 
-const App = () => {
-  return (
-    <div>
-      <Searchbar />
-      <ImageGallery />
-      <Loader />
-    </div>
-  );
-};
+import { ToastContainer } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 
-export default App;
+export default class App extends Component {
+  state = {
+    query: '',
+  };
+
+  handleSubmitForm = query => {
+    this.setState({ query });
+    console.log(query);
+  };
+
+  render() {
+    return (
+      <div>
+        <Searchbar onSubmit={this.handleSubmitForm} />
+        <ImageGallery query={this.state.query} />
+        <ToastContainer />
+      </div>
+    );
+  }
+}
