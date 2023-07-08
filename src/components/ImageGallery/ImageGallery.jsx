23aaa5ad4575349decd3181cfd3a7bcc41aaf70d
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-07-08 13:41:14 +0300

    Add styles to ImageGallery

--- src/components/ImageGallery/ImageGallery.jsx
+++ src/components/ImageGallery/ImageGallery.jsx
@@ -1,85 +1,83 @@
-import { Component } from 'react';
+import React, { Component } from 'react';
 
+import ButtonLoadMore from 'components/ButtonLoadMore/ButtonLoadMore';
+import ErrorCard from 'components/ErrorCard/ErrorCard';
 import ImageGalleryItem from 'components/ImageGalleryItem/ImageGalleryItem';
 import Loader from 'components/Loader/Loader';
-import notifyOptions from '../../helpers/toastNotifyOptions';
-import { API_KEY, BASE_URL } from '../../helpers/pixabayOptions';
+import NoResultCard from 'components/NoResultCard/NoResultCard';
 
+import notifyOptions from 'helpers/toastNotifyOptions';
 import { toast } from 'react-toastify';
+import { getImages } from 'services/api';
+import { ImageGalleryList } from './ImageGallery.styled';
 
-import axios from 'axios';
-
-export default class ImageGallery extends Component {
+class ImageGallery extends Component {
   state = {
-    queryData: null,
-    // isLoading: false,
-    error: null,
-    status: 'idle',
-    per_page: 40,
+    searchQuery: '',
+    images: null,
+    error: '',
+    isLoading: false,
     page: 1,
+    totalPages: 0,
   };
 
   componentDidUpdate(prevProps, prevState) {
-    const prevQuery = prevProps.query;
-    const nextQuery = this.props.query;
+    if (
+      prevProps.searchQuery !== this.props.searchQuery ||
+      prevState.page !== this.state.page
+    ) {
+      if (prevProps.searchQuery !== this.props.searchQuery) {
+        this.setState({ page: 1 });
+      }
+      if (this.props.searchQuery.length === 0) {
+        return;
+      }
+      this.setState({ isLoading: true });
+      getImages(this.props.searchQuery, this.state.page)
+        .then(response => {
+          if (response.status === 200 && response.data.hits !== 0) {
+            this.setState(prevState => ({
+              images:
+                this.state.page === 1
+                  ? response.data.hits
+                  : [...prevState.images, ...response.data.hits],
 
-    if (prevQuery !== nextQuery) {
-      this.setState({
-        status: 'pending',
-        // isLoading: true, queryData: null, error: null
-      });
-
-      axios
-        .get(BASE_URL, {
-          params: {
-            key: API_KEY,
-            q: nextQuery,
-            image_type: 'photo',
-            orientation: 'horizontal',
-            safesearch: true,
-            page: this.state.page,
-            per_page: this.state.per_page,
-          },
-        })
-        .then(resp => {
-          if (resp.data.hits.length) {
-            return this.setState({
-              queryData: resp.data,
-              status: 'resolved',
-            });
-          }
-          return Promise.reject(
-            new Error(`There is no images by query: ${nextQuery}`)
-          );
+              totalPages: Math.floor(response.data.totalHits / 20),
+            }));
+          } else return Promise.reject;
+          new Error(`Oops... there are no  images matching your search... `);
         })
         .catch(error => {
-          this.setState({ error, status: 'rejected' });
+          console.log(error.response.data);
+          this.setState({ error: error.response.data });
           toast.error('Oops. Something has gone wrong', notifyOptions);
+        })
+        .finally(() => {
+          this.setState({ isLoading: false });
         });
-      // .finally(() => this.setState({ isLoading: false }));
     }
   }
+
+  handleLoadMore = () => {
+    this.setState(prevState => ({ page: prevState.page + 1 }));
+    console.log(this.state.page);
+  };
+
   render() {
-    const { queryData, error, status } = this.state;
-
-    if (status === 'idle') {
-      return <div>Enter your query</div>;
-    }
-    if (status === 'pending') {
-      return <Loader />;
-    }
-
-    if (status === 'rejected') {
-      return <h2>{error.message}</h2>;
-    }
-
-    if (status === 'resolved') {
-      const imagesArray = queryData.hits;
-
-      return (
-        <div>
-          <ul className="gallery">
-            {imagesArray.map(({ id, webformatURL, largeImageURL, tags }) => {
+    const { images, isLoading, error, totalPages, page, searchQuery } =
+      this.state;
+    return (
+      <>
+        {isLoading && error && <ErrorCard>{error}</ErrorCard>}
+        {searchQuery.length === 0 &&
+          !isLoading &&
+          images &&
+          images.length === 0 && (
+            <NoResultCard>Oops! There are no images found...</NoResultCard>
+          )}
+        <ImageGalleryList>
+          {images &&
+            images.map(({ id, webformatURL, largeImageURL, tags }) => {
               return (
                 <ImageGalleryItem
                   key={id}
@@ -90,21 +88,16 @@ export default class ImageGallery extends Component {
                 />
               );
             })}
-          </ul>
-        </div>
-      );
-    }
-
-    // return (
-    //   <div>
-    //     {error && <h2>{error.message}</h2>}
-    //     {isLoading && <Loader />}
-    //     {queryData && !!queryData.totalHits && (
-    //       <ul className="gallery" style={{ backgroundColor: 'yellow' }}>
-    //         <ImageGalleryItem queryData={queryData} />
-    //       </ul>
-    //     )}
-    //   </div>
-    // );
+        </ImageGalleryList>
+        {isLoading && <Loader />}
+        {page < totalPages && !isLoading && (
+          <ButtonLoadMore onClick={this.handleLoadMore}>
+            Load more...
+          </ButtonLoadMore>
+        )}
+      </>
+    );
   }
 }
+
+export default ImageGallery;
