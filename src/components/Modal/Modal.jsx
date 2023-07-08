009ac5e9db344816c0a8bd41886f041471449917
oleksiilunin <116845999+oleksiilunin@users.noreply.github.com>
2023-07-08 13:40:04 +0300

    Add styles to Modal

--- src/components/Modal/Modal.jsx
+++ src/components/Modal/Modal.jsx
@@ -1,10 +1,15 @@
-const Modal = () => {
-  return (
-    <div className="overlay">
-      <div className="modal">
-        <img src="" alt="" />
-      </div>
-    </div>
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { ModalWrapper, Overlay } from './Modal.styled';
+
+const Modal = ({ imageURL, alt, onClickCloseModal }) => {
+  return ReactDOM.createPortal(
+    <Overlay onClick={onClickCloseModal}>
+      <ModalWrapper className="modal">
+        <img src={imageURL} alt={alt} />
+      </ModalWrapper>
+    </Overlay>,
+    document.body
   );
 };
 
